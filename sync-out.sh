aebd2be0162da48c762907cc146ef8c0aa31ea0c
Li-Wen Hsu <lwhsu@lwhsu.org>
2018-08-10 06:09:31 +0800

    Handle the case that adds new l10n ports

--- sync-out.sh
+++ sync-out.sh
@@ -5,6 +5,10 @@ LO_WC=~/freebsd-ports-libreoffice
 
 for p in `cat list.txt`
 do
-        rm -fr ${SVN_WC}/${p}/*
+	if [ -d ${SVN_WC}/${p}/ ]; then
+		rm -fr ${SVN_WC}/${p}/*
+	else
+		mkdir -p ${SVN_WC}/${p}
+	fi
         cp -R ${LO_WC}/${p}/* ${SVN_WC}/${p}/
 done
