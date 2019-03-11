a5a779a3c236c29dd705db920ba530a8013d180c
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-03-11 16:30:51 +0800

    Test in Cirrus-CI
    Obtained from: https://github.com/lwhsu/cirrus-porttest

---
+++ porttest.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+PORTSDIR=/usr/ports
+
+set -ex
+
+pwd
+
+cd /usr
+#ls -al ports
+mv ports ports.old
+#svnlite co svn://svn.freebsd.org/ports/head ports
+portsnap --interactive fetch extract
+
+for p in `cat list.txt`
+do
+	if [ -d ${PORTSDIR}/${p}/ ]; then
+		rm -fr ${PORTSDIR}/${p}/*
+	else
+		mkdir -p ${PORTSDIR}/${p}
+	fi
+        cp -R ./${p}/* ${PORTSDIR}/${p}/
+done
+
+mkdir /usr/ports/distfiles
+
+df -h
+
+#cd /usr/ports/editors/libreoffice
+#make all-depends-list | awk -F'/' '{print $4"/"$5}' | xargs pkg install -y
+
+echo "NO_ZFS=yes" >> /usr/local/etc/poudriere.conf
+echo "ALLOW_MAKE_JOBS=yes" >> /usr/local/etc/poudriere.conf
+sed -i.bak -e 's,FREEBSD_HOST=_PROTO_://_CHANGE_THIS_,FREEBSD_HOST=https://download.FreeBSD.org,' /usr/local/etc/poudriere.conf
+mkdir -p /usr/local/poudriere
+
+poudriere jail -c -j jail -v `uname -r`
+poudriere ports -c -f none -m null -M /usr/ports
+
+# use an easy port to bootstrap pkg repo
+poudriere bulk -t -j jail net/nc
+
+PORT=editors/libreoffice
+
+cd /usr/ports
+cd ${PORT}
+PWD=`pwd -P`
+PORTDIR=`dirname ${PWD}`
+PORTDIR=`dirname ${PORTDIR}`
+make all-depends-list | sed -e "s,${PORTDIR}/,," | xargs sudo pkg fetch -y -o pkgs
+rm -fr /usr/local/poudriere/data/packages/jail-default/.latest/All
+mv pkgs/All /usr/local/poudriere/data/packages/jail-default/.latest/
+rm -fr pkgs
+
+poudriere testport -j jail ${PORT}
