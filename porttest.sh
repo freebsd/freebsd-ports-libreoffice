bf356cf575e20340a8865baf6453d02dd9b24297
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-12-13 17:51:13 +0800

    Use `make missing`

--- porttest.sh
+++ porttest.sh
@@ -49,7 +49,7 @@ cd ${PORT}
 PWD=`pwd -P`
 PORTDIR=`dirname ${PWD}`
 PORTDIR=`dirname ${PORTDIR}`
-make all-depends-list | sed -e "s,${PORTDIR}/,," | xargs sudo pkg fetch -y -o pkgs
+sudo pkg fetch -y -o pkgs `make missing`
 rm -fr /usr/local/poudriere/data/packages/jail-default/.latest/All
 mv pkgs/All /usr/local/poudriere/data/packages/jail-default/.latest/
 rm -fr pkgs
