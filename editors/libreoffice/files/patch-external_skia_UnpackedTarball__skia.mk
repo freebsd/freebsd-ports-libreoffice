ae7a29db07150e3004051010461381e884736d81
Dima Panov <fluffy@FreeBSD.org>
2021-09-24 09:05:15 +1000

    Update to LibreOffice 7.2.2.RC1

--- editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
+++ editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
@@ -1,9 +1,10 @@
 --- external/skia/UnpackedTarball_skia.mk.orig	2021-02-05 14:00:19 UTC
 +++ external/skia/UnpackedTarball_skia.mk
-@@ -39,6 +39,7 @@ skia_patches := \
-     fast-png-write.patch.1 \
-     skia_sk_cpu_sse_level_0_by_default.patch.1 \
+@@ -41,6 +41,7 @@ skia_patches := \
      fix-warnings.patch.1 \
+     disable-freetype-colrv1.1 \
+     windows-libraries-system32.patch.1 \
 +    gfx-skia-skia-src-core-SkCpu.cpp.patch.0 \
  
  $(eval $(call gb_UnpackedTarball_set_patchlevel,skia,1))
+ 
