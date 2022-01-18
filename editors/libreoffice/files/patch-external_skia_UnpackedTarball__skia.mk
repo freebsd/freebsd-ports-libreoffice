3e09809b341a1014c391c23304e53acf75ee595d
Li-Wen Hsu <lwhsu@lwhsu.org>
2022-01-19 03:59:24 +0800

    Update distinfo and patches for 7.3 RC2

--- editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
+++ editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
@@ -1,9 +1,9 @@
---- external/skia/UnpackedTarball_skia.mk.orig	2021-02-05 14:00:19 UTC
-+++ external/skia/UnpackedTarball_skia.mk
-@@ -41,6 +41,7 @@ skia_patches := \
-     fix-warnings.patch.1 \
+--- external/skia/UnpackedTarball_skia.mk.orig	2022-01-13 03:09:02.000000000 +0800
++++ external/skia/UnpackedTarball_skia.mk	2022-01-19 03:55:20.647187000 +0800
+@@ -37,6 +37,7 @@
      disable-freetype-colrv1.1 \
      windows-libraries-system32.patch.1 \
+     fix-graphite-ifdef.patch.1 \
 +    gfx-skia-skia-src-core-SkCpu.cpp.patch.0 \
  
  $(eval $(call gb_UnpackedTarball_set_patchlevel,skia,1))
