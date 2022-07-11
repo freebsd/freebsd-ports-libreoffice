6c40a7c502491675d62485638bca34489e05f116
Dima Panov <fluffy@FreeBSD.org>
2022-07-12 02:17:45 +0300

    LibreOffice 7.4.0.RC1

--- editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
+++ editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
@@ -1,9 +1,9 @@
 --- external/skia/UnpackedTarball_skia.mk.orig	2022-05-12 13:26:05 UTC
 +++ external/skia/UnpackedTarball_skia.mk
-@@ -37,6 +37,7 @@ skia_patches := \
-     fix-graphite-ifdef.patch.1 \
+@@ -38,6 +38,7 @@ skia_patches := \
      allow-no-es2restrictions.patch.1 \
      vk_mem_alloc.patch.1 \
+     tdf148624.patch.1 \
 +    gfx-skia-skia-src-core-SkCpu.cpp.patch.0 \
  
  $(eval $(call gb_UnpackedTarball_set_patchlevel,skia,1))
