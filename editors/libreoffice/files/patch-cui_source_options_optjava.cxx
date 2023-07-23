e94e93aab4032472cb8b7671b62bd73281cf68cd
Dima Panov <fluffy@FreeBSD.org>
2023-07-23 15:58:30 +0300

    editors/libreoffice: adjust upstream no java option patch to 7.6.0.RC1
    Reported by:	poudriere failure

--- editors/libreoffice/files/patch-cui_source_options_optjava.cxx
+++ editors/libreoffice/files/patch-cui_source_options_optjava.cxx
@@ -10,19 +10,9 @@ ClassPathHdl_Impl, weld::Button&, void)  in
 cui/source/options/optjava.cxx ) anyway, see
 <https://gerrit.libreoffice.org/c/core/+/153386/2#message-06ee95be419a14ee8f08035142b5718e0377d120>.
 
-Change-Id: I0e5a21036ce048c18d408c07cce088dbab7fc030
-Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153396
-Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
-Tested-by: Jenkins
----
- cui/source/options/optjava.cxx | 4 ++--
- 1 file changed, 2 insertions(+), 2 deletions(-)
-
-diff --git cui/source/options/optjava.cxx cui/source/options/optjava.cxx
-index 7a537dfe4eed..64ba41c3fe0b 100644
 --- cui/source/options/optjava.cxx
 +++ cui/source/options/optjava.cxx
-@@ -935,12 +935,12 @@ OUString SvxJavaClassPathDlg::GetClassPath() const
+@@ -935,6 +935,7 @@ OUString SvxJavaClassPathDlg::GetClassPath() const
      return sPath.makeStringAndClear();
  }
  
@@ -30,22 +20,10 @@ index 7a537dfe4eed..64ba41c3fe0b 100644
  void SvxJavaClassPathDlg::SetClassPath( const OUString& _rPath )
  {
      if ( m_sOldPath.isEmpty() )
-         m_sOldPath = _rPath;
-     m_xPathList->clear();
--#if HAVE_FEATURE_JAVA
-     if (!_rPath.isEmpty())
-     {
-         std::vector paths = jfw_convertUserPathList(_rPath);
-@@ -962,8 +962,8 @@ void SvxJavaClassPathDlg::SetClassPath( const OUString& _rPath )
-         // select first entry
-         m_xPathList->select(0);
+@@ -963,5 +964,6 @@ void SvxJavaClassPathDlg::SetClassPath( const OUString
      }
--#endif
      SelectHdl_Impl(*m_xPathList);
  }
 +#endif
  
  /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- 
-2.41.0
-
