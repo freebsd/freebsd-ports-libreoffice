3cc6e32d7f408b576c5dcc4e2d1a4119b38becd8
Dima Panov <fluffy@FreeBSD.org>
2023-12-11 09:02:50 +0300

    editors/libreoffice: update skia patch

--- editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
+++ editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
@@ -1,10 +1,9 @@
 --- external/skia/UnpackedTarball_skia.mk.orig	2023-11-30 13:04:41 UTC
 +++ external/skia/UnpackedTarball_skia.mk
-@@ -40,6 +40,8 @@ skia_patches := \
-     redefinition-of-op.patch.0 \
-     0001-Added-missing-include-cstdio.patch \
-     0001-AvoidCombiningExtrememelyLargeMeshes.patch.1 \
-+    gfx-skia-skia-src-core-SkCpu.cpp.patch.0 \
+@@ -40,6 +40,7 @@ skia_patches := \
+     incomplete.patch.0 \
+     ubsan-missing-typeinfo.patch.1 \
+     incomplete-type-SkImageGenerator.patch.1 \
 +    powerpc64le-skia.patch.0 \
  
  $(eval $(call gb_UnpackedTarball_set_patchlevel,skia,1))
