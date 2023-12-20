24c81f48f0e152403437158115c74773a20dcf34
Dima Panov <fluffy@FreeBSD.org>
2023-12-20 07:48:35 +0300

    Libreoffice 24.2.0.0.beta1

--- editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
+++ editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
@@ -1,9 +1,9 @@
---- external/skia/UnpackedTarball_skia.mk.orig	2023-11-30 13:04:41 UTC
+--- external/skia/UnpackedTarball_skia.mk.orig	2023-12-08 15:11:48 UTC
 +++ external/skia/UnpackedTarball_skia.mk
-@@ -40,6 +40,7 @@ skia_patches := \
-     incomplete.patch.0 \
+@@ -41,6 +41,7 @@ skia_patches := \
      ubsan-missing-typeinfo.patch.1 \
      incomplete-type-SkImageGenerator.patch.1 \
+     0001-AvoidCombiningExtrememelyLargeMeshes.patch.1 \
 +    powerpc64le-skia.patch.0 \
  
  $(eval $(call gb_UnpackedTarball_set_patchlevel,skia,1))
